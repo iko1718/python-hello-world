630061a4f24c0be30e4c3e30408f559b6d296e84
iko1718 <sonaliponnappa@gmail.com>
2025-05-15 17:42:32 +0530

    Initial commit - Python Hello World with tests

---
+++ app.py
@@ -0,0 +1,1 @@
+def add(a, b):\n    return a + b\n\nif __name__ == '__main__':\n    print('Sum of 2 and 3 is:', add(2, 3))
---
+++ tests/test_app.py
@@ -0,0 +1,1 @@
+from app import add\n\ndef test_add():\n    assert add(2, 3) == 5\n    assert add(0, 0) == 0
