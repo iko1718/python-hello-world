a58be2233a83e2da832b716afb5aa3d324a0492c
Sonali Ponnappa A <131168414+iko1718@users.noreply.github.com>
2025-05-15 18:05:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
-def add(a, b):\n    return a + b\n\nif __name__ == '__main__':\n    print('Sum of 2 and 3 is:', add(2, 3))
+from app import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(0, 0) == 0
